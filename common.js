a8f7f31d7c0e371c343297f2ec294feb49b29196
maxlath <k@maxlath.eu>
2018-05-11 08:58:52 +0200

    added missing file

---
+++ common.js
@@ -0,0 +1,22 @@
+module.exports = {
+  addScriptHeader: () => {
+    console.log('#!/usr/bin/env sh')
+    console.log('set -eu')
+  },
+
+  buildCommand: (cmd, statementId, property, value) => {
+    console.log(`echo '${statementId}' ${property} ${value}`)
+    console.log(`wd ${cmd} '${statementId}' ${property} ${value} | jd success`)
+  },
+
+  // From the SPARQL results format
+  fixStatementId: statementId => {
+    var [ qid, ...rest ] = statementId.split(/-/)
+    qid = rest.join('-')
+    return `${qid}$${rest}`
+  },
+
+  addDone: () => {
+    console.log('echo done')
+  }
+}
