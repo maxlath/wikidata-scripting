ea94ea6a147a615dd16162f5a68dc27e83ecfd93
maxlath <k@maxlath.eu>
2025-08-19 23:29:15 +0200

    format isbns: run fixes on all the isbns

--- format_isbns/run.sh
+++ format_isbns/run.sh
@@ -6,19 +6,19 @@ cd /home/maxlath/code/wikidata-scripting/isbns-format
 {
   date --iso-8601=seconds
 
-  echo "Get invalid_isbns_13.ndjson"
-  wd sparql invalid_isbn_13_format.rq | jq '.[]' -cr > invalid_isbns_13.ndjson
-  echo "Get invalid_isbns_10.ndjson"
-  wd sparql invalid_isbn_10_format.rq | jq '.[]' -cr > invalid_isbns_10.ndjson
+  echo "Get all_isbns_13.ndjson"
+  wd sparql all_isbns_13.rq | jq '.[]' -cr > all_isbns_13.ndjson
+  echo "Get all_isbns_10.ndjson"
+  wd sparql all_isbns_10.rq | jq '.[]' -cr > all_isbns_10.ndjson
 
   echo "Build move_claim_batch.moveFromIsbn13ToIsbn10.ndjson"
-  cat invalid_isbns_13.ndjson | ndjson-apply ./fix_isbn.js moveFromIsbn13ToIsbn10 > move_claim_batch.moveFromIsbn13ToIsbn10.ndjson
+  cat all_isbns_13.ndjson | ndjson-apply ./fix_isbn.js moveFromIsbn13ToIsbn10 > move_claim_batch.moveFromIsbn13ToIsbn10.ndjson
   echo "Build move_claim_batch.moveFromIsbn10ToIsbn13.ndjson"
-  cat invalid_isbns_10.ndjson | ndjson-apply ./fix_isbn.js moveFromIsbn10ToIsbn13 > move_claim_batch.moveFromIsbn10ToIsbn13.ndjson
+  cat all_isbns_10.ndjson | ndjson-apply ./fix_isbn.js moveFromIsbn10ToIsbn13 > move_claim_batch.moveFromIsbn10ToIsbn13.ndjson
   echo "Build update_claim_batch.fixIsbn13.ndjson"
-  cat invalid_isbns_13.ndjson | ndjson-apply ./fix_isbn.js fixIsbn13 > update_claim_batch.fixIsbn13.ndjson
+  cat all_isbns_13.ndjson | ndjson-apply ./fix_isbn.js fixIsbn13 > update_claim_batch.fixIsbn13.ndjson
   echo "Build update_claim_batch.fixIsbn10.ndjson"
-  cat invalid_isbns_10.ndjson | ndjson-apply ./fix_isbn.js fixIsbn10 > update_claim_batch.fixIsbn10.ndjson
+  cat all_isbns_10.ndjson | ndjson-apply ./fix_isbn.js fixIsbn10 > update_claim_batch.fixIsbn10.ndjson
 
   echo "Send move_claim_batch.moveFromIsbn13ToIsbn10.ndjson batch edits"
   cat move_claim_batch.moveFromIsbn13ToIsbn10.ndjson | wd mc -b --no-exit-on-error --summary 'move ISBN-10 from P212 to P957'
